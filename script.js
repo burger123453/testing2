bc0a01f3383d13b99740e163fd8b5522b57f878e
burger123453 <125590920+burger123453@users.noreply.github.com>
2023-11-17 22:52:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+let kumo="";
+function bob() {
+    if(kumo==="dog") {
+        document.getElementById("kumoooo").innerHTML="kumoooo *dog noises*";}
+    if(kumo==="cloud") {
+        document.getElementById("kumoooo").innerHTML="kuummmmoooooo *cloud rains*";
+    }   
+}
+function dog() {
+    kumo="dog";
+}
+function cloud() {
+    kumo="cloud";
+}
